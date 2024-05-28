7bc64084c072131529cdaca4d12c835114004fcd
Valentine Orga <valentineorga@gmail.com>
2024-05-28 10:41:07 +0100

    feat: setup deploy scripts

--- solidity/scripts/Deploy.sol
+++ solidity/scripts/Deploy.sol
@@ -1,20 +1,58 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
 import {Script} from 'forge-std/Script.sol';
-import {ICryptoAnts, CryptoAnts} from 'contracts/tokens/CryptoAnts.sol';
-import {IEgg, Egg} from 'contracts/tokens/Egg.sol';
+import {GovernanceToken} from "contracts/governance/GovernanceToken.sol";
+import {GovernanceTimeLock} from "contracts/governance/GovernanceTimeLock.sol";
+import {GovernorContract} from "contracts/governance/GovernorContract.sol";
+import {CryptoAnts} from "contracts/tokens/CryptoAnts.sol";
+import {Egg} from "contracts/tokens/Egg.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
+error InvalidPrivateKey(string);
+error TransferFailed();
 contract Deploy is Script {
-  ICryptoAnts internal _cryptoAnts;
   address deployer;
-  IEgg internal _eggs;
+
+  address private constant LINK_ADDRESS = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+  uint256 private constant AMOUNT_TO_FUND_ANTS = 5;
+
+  uint256 constant MIN_DELAY = 1;
+  address[] proposers;
+  address[] executors;
 
   function run() external {
     deployer = vm.rememberKey(vm.envUint('DEPLOYER_PRIVATE_KEY'));
+    if(deployer == address(0)) revert InvalidPrivateKey("You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env");
+
     vm.startBroadcast(deployer);
-    // _eggs = new Egg(address(_cryptoAnts));
-    // _cryptoAnts = new CryptoAnts(address(_eggs));
+
+    GovernanceToken governanceToken = new GovernanceToken();
+
+    governanceToken.delegate(deployer);
+
+    GovernanceTimeLock governanceTimeLock = new GovernanceTimeLock(MIN_DELAY, proposers, executors);
+
+    GovernorContract governorContract = new GovernorContract(governanceToken, governanceTimeLock);
+
+    bytes32 proposerRole = governanceTimeLock.PROPOSER_ROLE();
+    bytes32 executorRole = governanceTimeLock.EXECUTOR_ROLE();
+    bytes32 timelockAdminRole = governanceTimeLock.DEFAULT_ADMIN_ROLE();
+
+    governanceTimeLock.grantRole(proposerRole, address(governorContract));
+    governanceTimeLock.grantRole(executorRole, address(0));
+    governanceTimeLock.revokeRole(timelockAdminRole, deployer);
+
+    Egg egg = new Egg();
+
+    CryptoAnts ants = new CryptoAnts(address(egg), address(governanceTimeLock));
+
+    LinkTokenInterface link = LinkTokenInterface(LINK_ADDRESS);
+
+    if(link.transfer(address(ants), AMOUNT_TO_FUND_ANTS) == false) revert TransferFailed();
+
+    egg.initialize(address(ants));
+
     vm.stopBroadcast();
   }
 }
